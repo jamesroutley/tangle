fd3ef66b15fd4eca9c970dda9c93ff87539398bd
James Routley <jroutley@gmail.com>
2020-06-24 08:45:57 +0100

    Remove nested arrays

--- tangle/tangle.go
+++ tangle/tangle.go
@@ -28,22 +28,22 @@ func NewTangler(options ...TanglerOption) *Tangler {
 }
 
 func (t *Tangler) Tangle(sourceFiles ...string) ([]byte, error) {
-	allBlocks, err := extractAllBlocks(sourceFiles)
+	blocks, err := extractBlocksFromFiles(sourceFiles)
 	if err != nil {
 		return nil, err
 	}
 
-	allFilteredBlocks := filterBlocks(allBlocks, t.filters)
+	filteredBlocks := filterBlocks(blocks, t.filters)
 
 	var orderedBlocks []*CodeBlock
 	if len(t.order) > 0 {
 		var err error
-		orderedBlocks, err = explicitlyOrderBlocks(allFilteredBlocks, t.order)
+		orderedBlocks, err = explicitlyOrderBlocks(filteredBlocks, t.order)
 		if err != nil {
 			return nil, err
 		}
 	} else {
-		orderedBlocks = orderBlocks(allFilteredBlocks)
+		orderedBlocks = orderBlocks(filteredBlocks)
 	}
 
 	// Generate output
@@ -56,80 +56,18 @@ func (t *Tangler) Tangle(sourceFiles ...string) ([]byte, error) {
 	return bytes.TrimSuffix(output.Bytes(), []byte("\n")), nil
 }
 
-func extractAllBlocks(sourceFiles []string) (allBlocks [][]*CodeBlock, err error) {
+func extractBlocksFromFiles(sourceFiles []string) (allBlocks []*CodeBlock, err error) {
 	for _, source := range sourceFiles {
-		blocks, err := getCodeBlocksFromFile(source)
+		blocks, err := extractBlocksFromFile(source)
 		if err != nil {
 			return nil, err
 		}
-		allBlocks = append(allBlocks, blocks)
+		allBlocks = append(allBlocks, blocks...)
 	}
 	return allBlocks, nil
 }
 
-func filterBlocks(allBlocks [][]*CodeBlock, filters []Filter) (allFilteredBlocks [][]*CodeBlock) {
-	for _, file := range allBlocks {
-		var filteredBlocks []*CodeBlock
-		for _, block := range file {
-			if !allFilters(block, filters...) {
-				continue
-			}
-			filteredBlocks = append(filteredBlocks, block)
-		}
-		if len(filteredBlocks) > 0 {
-			allFilteredBlocks = append(allFilteredBlocks, filteredBlocks)
-		}
-	}
-	return allFilteredBlocks
-}
-
-func orderBlocks(blocks [][]*CodeBlock) (orderedBlocks []*CodeBlock) {
-	namedBlockIndex := map[string]int{}
-	for _, file := range blocks {
-		for _, block := range file {
-			if block.Name != "" {
-				index, ok := namedBlockIndex[block.Name]
-				if ok {
-					// We've previously seen a block with this name, replace it
-					orderedBlocks[index] = block
-					continue
-				}
-				// else write it to the list and store the index
-				orderedBlocks = append(orderedBlocks, block)
-				namedBlockIndex[block.Name] = len(orderedBlocks) - 1
-				continue
-			}
-			// Unnamed block, write it to the list
-			orderedBlocks = append(orderedBlocks, block)
-		}
-	}
-	return orderedBlocks
-}
-
-func explicitlyOrderBlocks(
-	blocks [][]*CodeBlock, order []string,
-) (orderedBlocks []*CodeBlock, err error) {
-	blocksByName := map[string]*CodeBlock{}
-	for _, file := range blocks {
-		for _, block := range file {
-			if block.Name == "" {
-				continue
-			}
-			blocksByName[block.Name] = block
-		}
-	}
-
-	for _, name := range order {
-		block, ok := blocksByName[name]
-		if !ok {
-			return nil, fmt.Errorf("the order specifies the name %s, but there's not block with that order", name)
-		}
-		orderedBlocks = append(orderedBlocks, block)
-	}
-	return orderedBlocks, nil
-}
-
-func getCodeBlocksFromFile(filename string) ([]*CodeBlock, error) {
+func extractBlocksFromFile(filename string) ([]*CodeBlock, error) {
 	source, err := ioutil.ReadFile(filename)
 	if err != nil {
 		return nil, err
@@ -165,4 +103,56 @@ func getCodeBlocksFromFile(filename string) ([]*CodeBlock, error) {
 	}
 
 	return codeBlocks, nil
+}
+
+func filterBlocks(blocks []*CodeBlock, filters []Filter) (filtered []*CodeBlock) {
+	for _, block := range blocks {
+		if !allFilters(block, filters...) {
+			continue
+		}
+		filtered = append(filtered, block)
+	}
+	return filtered
+}
+
+func orderBlocks(blocks []*CodeBlock) (orderedBlocks []*CodeBlock) {
+	namedBlockIndex := map[string]int{}
+	for _, block := range blocks {
+		if block.Name != "" {
+			index, ok := namedBlockIndex[block.Name]
+			if ok {
+				// We've previously seen a block with this name, replace it
+				orderedBlocks[index] = block
+				continue
+			}
+			// else write it to the list and store the index
+			orderedBlocks = append(orderedBlocks, block)
+			namedBlockIndex[block.Name] = len(orderedBlocks) - 1
+			continue
+		}
+		// Unnamed block, write it to the list
+		orderedBlocks = append(orderedBlocks, block)
+	}
+	return orderedBlocks
+}
+
+func explicitlyOrderBlocks(
+	blocks []*CodeBlock, order []string,
+) (orderedBlocks []*CodeBlock, err error) {
+	blocksByName := map[string]*CodeBlock{}
+	for _, block := range blocks {
+		if block.Name == "" {
+			continue
+		}
+		blocksByName[block.Name] = block
+	}
+
+	for _, name := range order {
+		block, ok := blocksByName[name]
+		if !ok {
+			return nil, fmt.Errorf("the order specifies the name %s, but there's not block with that order", name)
+		}
+		orderedBlocks = append(orderedBlocks, block)
+	}
+	return orderedBlocks, nil
 }
